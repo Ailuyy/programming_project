c4a4a129434e7c22103de0b149b442c334d08868
Julia <julialtnc@gmail.com>
2025-11-16 19:32:19 +0100

    CU-86c6j0fv8[complete] test - calculate_discount function

--- functions/calculate_discount.py
+++ functions/calculate_discount.py
@@ -1,4 +1,4 @@
 def calculate_discount(price: float, discount: float) -> float:
-    if not discount > 0 and not discount < 1:
+    if discount < 0.0 or discount > 1.0:
         raise ValueError('Discount must be between 0 and 1')
     return price * (1 - discount)
